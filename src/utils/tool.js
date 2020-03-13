9113420a1a936f8af1958eba99188e2ca4cdf775
ko tainen <TonyK@hyvesolutions.com>
2020-03-13 23:16:25 +0800

    Add tool.js

---
+++ src/utils/tool.js
@@ -0,0 +1,16 @@
+import padStart from "lodash/padStart";
+
+const ms2Time = (milliseconds)=>
+{
+    let time = milliseconds;
+    const ms = time % 1000;
+    time = (milliseconds-ms) / 1000;
+    const second = time % 60;
+    time = (time-second) / 60;
+    const minute = time % 60;
+    const hour = (time-minute) / 60;
+    const result= padStart(hour, 2, '0') + ":" + padStart(minute, 2, '0') + ":" + padStart(second, 2, '0') + ":" + padStart(ms, 3, '0');
+    return result;
+};
+
+export default ms2Time;
