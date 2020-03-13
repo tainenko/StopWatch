61d0283412fffcf9b931b652bd78a46dd877e9c8
ko tainen <TonyK@hyvesolutions.com>
2020-03-13 23:11:05 +0800

    add SplitTimes component

---
+++ src/components/SplitTimes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MajorClock from "./MajorClock";
+import PropTypes from "prop-types";
+
+const SplitTimes = ({value = []}) => {
+    return value.map((v, k) => (
+        <MajorClock key={k} milliseconds={v}/>
+    ))
+};
+
+SplitTimes.propTypes={
+    value:PropTypes.array.isRequired,
+};
+
+export default SplitTimes;
